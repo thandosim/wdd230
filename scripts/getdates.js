43b56993b7ac90ec8e7c0264e35dd8a81a6404d0
thandosim <sim24001@byui.edu>
2025-03-13 21:58:22 +0200

    fixed hamburger

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,3 +1,12 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
+
+
 var date = new Date();
 var year = date.getFullYear();
 document.getElementById("currentyear").innerHTML = year;
