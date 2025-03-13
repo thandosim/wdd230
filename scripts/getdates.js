5ba83f5099bee46cd355c26cbc38dc4a48532975
thandosim <sim24001@byui.edu>
2025-03-13 22:19:43 +0200

    added dark mode

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,3 +1,26 @@
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+const cards = document.querySelectorAll(".card");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🕶️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "🔆";
+        cards.forEach(card => {
+            card.style.background = "#222";
+        });
+
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "🕶️";
+        cards.forEach(card => {
+            card.style.background = "#f4f4f4";
+        });
+	}
+});
+
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
 
