ddcb5080507fc32c28c866d3a9c2dcb99a68afe7
thandosim <sim24001@byui.edu>
2025-03-13 19:47:50 +0200

    chamber home page large done

---
+++ chamber/scripts/chamber.js
@@ -0,0 +1,14 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
+
+var date = new Date();
+var year = date.getFullYear();
+document.getElementById("currentyear").innerHTML = year;
+
+document.getElementById("lastModified").textContent = document.lastModified;
+
