67d84b553696bd34083e46866f23ff0560711d2c
thandosim <sim24001@byui.edu>
2025-03-20 11:20:52 +0200

    finished lazyload

---
+++ week03/scripts/lazyload.js
@@ -0,0 +1,4 @@
+var date = new Date();
+var year = date.getFullYear();
+document.getElementById("currentyear").innerHTML = year;
+document.getElementById("lastModified").textContent = document.lastModified;
