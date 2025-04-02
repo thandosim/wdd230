0ad555fdaa0365d672eacbc0c8106c0b136b9d91
thandosim <sim24001@byui.edu>
2025-04-02 09:53:41 +0200

    trying dynamic links

---
+++ scripts/links.js
@@ -0,0 +1,38 @@
+const baseURL = "https://thandosim.github.io/wdd230/";
+const linksURL = "https://thandosim.github.io/wdd230/data/links.json";
+
+const weeks = document.querySelector("#weeks");
+
+async function getLinks() {
+    try {
+        const response = await fetch(linksURL);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayLinks(data)
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getLinks();
+
+const displayLinks = (data) => {
+    data.forEach((week) => {
+        let weekItem = document.createElement("li");
+        weekItem.textContent = `${week.week} :`;
+        week.forEach((link) => {
+            let url = document.createElement("a");
+            url.setAttribute('href', link.url);
+            url.textContent = `${link.title}`;
+
+            weekItem.appendChild(url);        
+        })
+
+        weeks.appendChild(weekItem);
+
+    });
+}
