124bbc65d2b436da2330f6ec5cd2f9627b90a75c
thandosim <sim24001@byui.edu>
2025-04-02 08:27:56 +0200

    finished part 1

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -51,6 +51,40 @@ function validatePasswords() {
 }
 
 
+const currentTemp = document.querySelector('#weather-info');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=-26.317&lon=31.133&units=metric&appid=9ff9d7b1bf21bb9e41447a69abf491e6';
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data)
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+    
+apiFetch();
+
+function displayResults(data) { 
+
+    currentTemp.innerHTML = `${data.main.temp}&deg;C`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+}
+
+
 const visitsDisplay = document.querySelector("#visit-count");
 let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
 
